d6f77d544138c70e0676d5cd127b8d53240c6299
Vishnu R <visvishnuvis@gmail.com>
2023-12-13 15:22:01 +0530

    initial commit with stars icon based on rating

--- script.js
+++ script.js
@@ -3,6 +3,32 @@
 
 // <!-- vishnu -->
 
+document.addEventListener("DOMContentLoaded", function () {
+    const ratingContainers = document.querySelectorAll('.rating-container');
+
+    ratingContainers.forEach(container => {
+        const textContent = container.textContent.trim();
+        const ratingValue = parseFloat(textContent);
+
+        if (!isNaN(ratingValue) && ratingValue >= 1 && ratingValue <= 5) {
+            displayRating(container, ratingValue);
+        } else {
+            console.error(`Invalid rating value `);
+        }
+    });
+
+    function displayRating(container, value) {
+
+        for (let i = 1; i <= 5; i++) {
+            const star = document.createElement('span');
+            star.className = 'rating-star ' + (i <= value ? 'active' : '');
+            star.innerHTML = '&#9733;'; 
+            container.appendChild(star);
+        }
+    }
+});
+
+
 
 // <!-- yogesh -->
 
