0c4dd1348e870b55dfe011751a943847b1c6f361
Utkarsh Agnihotri <utkarshagnihotri7@gmail.com>
2023-12-16 23:57:55 +0530

    booking page done

---
+++ navbar.js
@@ -0,0 +1,20 @@
+
+let nav=` 
+<a href="./index.html" class="logo">
+<img src="clear logo.png"  width:100px" alt="logo">
+</a>
+<ul class="nav-list">
+  <li><a href="">Home</a></li>
+  <li><a href="./about.html">About</a></li>
+  <li><a href="./services.html">Services</a></li>
+  <li><a href="#">Your Package</a></li>
+</ul>
+<div class="nav-actions">
+  <a href="./login.html" class="button loginBtn">Login</a>
+  <a href="./register.html" class="button registerBtn">Register</a>
+</div>
+`
+
+export {nav};
+
+
